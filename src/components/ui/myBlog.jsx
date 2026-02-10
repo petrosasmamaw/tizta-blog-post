1b04bc0873df4e56464e15ea6b8b6ca6b445c79b
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-02-10 14:11:13 -0400

    Update myBlog.jsx

--- src/components/ui/myBlog.jsx
+++ src/components/ui/myBlog.jsx
@@ -23,7 +23,7 @@ export default function MyBlog() {
     setNewComment("");
   };
 
-  if (!selectedBlog) return <p>Loading.</p>;
+  if (!selectedBlog) return <p>Loading..</p>;
 
   return (
     <div className="myblog-container">
