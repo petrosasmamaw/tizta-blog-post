5ab93018ca91bce377a635e94319a3d12100eaa2
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-02-27 21:23:09 +0300

    Update home.jsx

--- src/components/ui/home.jsx
+++ src/components/ui/home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchBlogs } from "../slice/blogSlice";
 import { Link } from "react-router-dom";
-..
+...
 export default function Home({user}) {
   const dispatch = useDispatch();
   const { blogs, status } = useSelector((state) => state.blog);
