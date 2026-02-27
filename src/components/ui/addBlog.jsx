87b125d773fcd9211504eddd8119509e4576bcdb
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-02-27 21:21:34 +0300

    Update addBlog.jsx

--- src/components/ui/addBlog.jsx
+++ src/components/ui/addBlog.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addBlog } from "../slice/blogSlice";
-
+.
 export default function AddBlog({ userId }) {
   const [username, setUsername] = useState("");
   const [description, setDescription] = useState("");
