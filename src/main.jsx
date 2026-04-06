1ec15882c0ab2edec39fc39a24beba0a8dbc2e81
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-04-06 22:00:59 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import  store  from './store.js';
-1234567
+12345678
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
